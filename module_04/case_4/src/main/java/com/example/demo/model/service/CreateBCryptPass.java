1126e7d01d92de810db856e7dbc353b34c3465d9
vantjn01 <levandiep1991@gmail.com>
2021-09-16 15:15:40 +0700

    module 4

---
+++ module_04/case_4/src/main/java/com/example/demo/model/service/CreateBCryptPass.java
@@ -0,0 +1,12 @@
+package com.example.demo.model.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/*File này dùng để tạo pass đã được mã hóa*/
+public class CreateBCryptPass {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("123"));
+    }
+}
+

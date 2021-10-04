76732be3ce0c47c0470c463a0dca385160813245
vantjn01 <levandiep1991@gmail.com>
2021-10-04 20:52:29 +0700

    module 5

--- module_05/caseStudy/caseStudy/src/app/app.component.ts
+++ module_05/caseStudy/caseStudy/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Customer} from "../model/Customer";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'caseStudy';
+  customers: Customer[] = [
+    {idCustomer: 1, idTypeCustomer: 1, name: "Lê Văn Diệp", dateOfBirth: "1991-4-15", identityCard: 197266943,
+      phone: 987654321, mail: "levandiep1991@gmail.com", address: "QT"},
+    {idCustomer: 2, idTypeCustomer: 2, name: "Đồng Văn Nhật", dateOfBirth: "1999-4-11", identityCard: 197266943,
+      phone: 987654321, mail: "levandiep1991@gmail.com", address: "QT"},
+    {idCustomer: 3, idTypeCustomer: 3, name: "Nguyễn Thanh Hà", dateOfBirth: "1991-4-15", identityCard: 197266943,
+      phone: 987654321, mail: "levandiep1991@gmail.com", address: "QT"},
+    {idCustomer: 4, idTypeCustomer: 4, name: "Hồ Kỳ Tài", dateOfBirth: "1991-4-15", identityCard: 197266943,
+      phone: 987654321, mail: "levandiep1991@gmail.com", address: "QT"},
+  ]
 }
---
+++ module_05/caseStudy/caseStudy/src/model/Customer.ts
@@ -0,0 +1,11 @@
+export interface Customer {
+  idCustomer: number;
+  idTypeCustomer: number;
+  name: string;
+  dateOfBirth: string;
+  identityCard: number;
+  phone: number;
+  mail: string;
+  address: string;
+
+}
